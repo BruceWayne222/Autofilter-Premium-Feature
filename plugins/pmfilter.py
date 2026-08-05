3735b313ecfad0d94879583ec140baec1a09b722
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2026-08-05 15:05:17 +0530

    Add files via upload

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1369,7 +1369,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('‼️ ᴅɪꜱᴄʟᴀɪᴍᴇʀ ‼️', callback_data='disclaimer'),
-            InlineKeyboardButton ('🪔 sᴏᴜʀᴄᴇ', callback_data='source'),
         ],[
             InlineKeyboardButton('ᴅᴏɴᴀᴛɪᴏɴ 💰', callback_data='donation'),
         ],[
