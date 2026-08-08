f83f454ccdc529f2378dbfc1aa01adb7296d39d3
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2026-08-08 19:01:32 +0530

    Add files via upload

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1785,7 +1785,7 @@ async def advantage_spell_chok(client, message):
     if not movies:
         google = search.replace(" ", "+")
         button = [[InlineKeyboardButton(
-            "🔍 ᴄʜᴇᴄᴋ sᴘᴇʟʟɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ 🔍", url=f"https://www.google.com/search?q={google}")]]
+            "🔍 Google හි Spelling පරීක්ෂා කරන්න 🔍", url=f"https://www.google.com/search?q={google}")]]
         k = await message.reply_text(text=script.I_CUDNT.format(search), reply_markup=InlineKeyboardMarkup(button))
         await asyncio.sleep(60)
         await k.delete()
